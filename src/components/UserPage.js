e967656f728094e8edf0cf999c549ccb98dad3ed
Chidiebere Nwigwe <chidonnwigz@gmail.com>
2024-12-02 14:17:02 -0700

    Updated User Page to Display books from backend and also updated styles to show differnet background for home page and administrator page

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,7 +1,58 @@
 import Header from "./Header";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from './HomePage.module.css';
+
 const UserPage = () => {
+
+    const [books, setBooks] = useState([]);
+    const [error, setError] = useState('');
+
+    const FetchBooks = async () =>{
+        try {
+        const response = await fetch('http://localhost:7000/books', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }          
+        });
+    
+        if (!response.ok) {
+          // Handle failure
+          const data = await response.json();
+          setError(data.message || 'CANNOT GET BOOKS');
+          return;
+        }
+        const data = await response.json();
+        console.log(data);
+        setBooks(data); 
+        setError('');
+      } catch (err) {
+        console.error('Getting Books failed', err);
+        setError('An error occurred while getting books');
+      } 
+    }
+
+    useEffect(()=>{
+        FetchBooks();
+    }, [])
+
     return ( 
-        <Header>USER WILL BE WORKED ON BY ANTHONY AND CHIDI</Header>
+        <div>
+            {
+                books.map((book) => (
+                    <div key={book.id}>
+                        <Link className={styles.link}>
+                            <img src={book.coverImage} alt="Product Image"/>
+                            <h1>Title: {book.title}</h1>
+                            <h2>Author: {book.author}</h2>
+                            <p>Description: {book.description}</p>
+                            <p>Publication Date: {book.publicationDate}</p>
+                        </Link>               
+                    </div>
+                ))
+            }      
+        </div>
 
      );
 }
